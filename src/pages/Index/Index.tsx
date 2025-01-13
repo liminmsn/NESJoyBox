fd81c3bb7338e1ba4ba20426fae8ad90a1d40f3a
杨再涛 <1772081518@qq.com>
2025-01-13 17:57:33 +0800

    修复

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,12 +1,9 @@
 import { IonButton, IonContent } from "@ionic/react";
 import Alert from "../../plugin/Alert";
-import Camera from "../../plugin/Camera";
 
 export default function Index() {
     async function clickMe(item: number) {
         if (item == 0) {
-            const img_data = await Camera.taking_photos();
-            console.log(img_data);
         }
         if (item == 1) {
             const val = await Alert.echo({ value: "Hello World" });
--- src/plugin/Camera.ts
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-interface CameraType {
-    taking_photos(): Promise<{ value: any }>;
-}
-
-const Camera = registerPlugin<CameraType>('Camera');
-export default Camera;
