66f02b4d3316652268f79b6c36b3f71efa3d2b9d
杨再涛 <1772081518@qq.com>
2025-01-17 18:09:53 +0800

    添加可播放视频源

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,29 +1,78 @@
-import { IonButton, IonContent } from "@ionic/react";
-import { Alert, Router } from "../../plugin/Plugins";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonRow,
+  IonText,
+} from "@ionic/react";
+import { Router } from "../../plugin/Plugins";
+import { getM3u, M3uItem } from "../../tool/loadFile";
+import { useEffect, useState } from "react";
 import { Capacitor } from "@capacitor/core";
+import "./style.css";
 
 export default function Index() {
-  async function clickMe(item: number) {
+  /**播放url链接视频 */
+  async function onPlay(url: string) {
+    console.log(url);
     if (Capacitor.isNativePlatform()) {
-      let val;
-      switch (item) {
-        case 0:
-          val = await Router.openPage({
-            path: "HLSPlay",
-            obj: { url: "https://live.wjyanghu.com/live/CH1.m3u8" },
-          });
-          break;
-        case 1:
-          val = await Alert.showTips({ value: "Hello World" });
-          console.log(val);
-          break;
-      }
+      await Router.openPage({
+        path: "HLSPlay",
+        obj: { url: url },
+      });
     }
   }
+
+  const [state, setState] = useState<M3uItem[][]>([]);
+  useEffect(() => {
+    return function () {
+      getM3u().then((res) => {
+        const grid = 3;
+        const grid_arr = [] as any[];
+        for (let i = 0; i < res.length; i += grid) {
+          grid_arr.push(res.slice(i, i + grid));
+        }
+        setState(grid_arr);
+      });
+    };
+  }, []);
   return (
-    <IonContent>
-      <IonButton onClick={() => clickMe(0)}>播放推流视频</IonButton>
-      <IonButton onClick={() => clickMe(1)}>显示弹窗</IonButton>
+    <IonContent className="Index">
+      <IonGrid>
+        {state.map((item, index) => {
+          return (
+            <IonRow key={index}>
+              {item.map((card, i) => {
+                return (
+                  <IonCol
+                    key={i}
+                    size={String(12 / item.length)}
+                    onClick={() => onPlay(card.url)}
+                  >
+                    <IonCard>
+                      <IonCardHeader>
+                        <IonCardTitle>{card.name}</IonCardTitle>
+                      </IonCardHeader>
+                      <IonCardContent>
+                        <IonText>{card.url}</IonText>
+                      </IonCardContent>
+                    </IonCard>
+                  </IonCol>
+                );
+              })}
+            </IonRow>
+          );
+        })}
+      </IonGrid>
+      {/* <IonButton onClick={() => clickMe(0)}>播放推流视频</IonButton>
+      <IonButton onClick={() => clickMe(1)}>显示弹窗</IonButton> */}
     </IonContent>
   );
 }
---
+++ src/tool/loadFile.ts
@@ -0,0 +1,39 @@
+export async function loadLocationFile(url: string) {
+  const res = await fetch(url, { method: "GET" });
+  if (res.ok && res.headers.get("content-type") !== "text/html") {
+    return res;
+  }
+  return { ok: false, data: { info: "Error fetching the file!" } };
+}
+
+export async function getM3u() {
+  const res = await loadLocationFile("/m3u/live_ok.m3u");
+  if (res.ok && res instanceof Response) {
+    const lines = (await res.text()).split("\n");
+    const m3u_arr: M3uItem[] = [];
+
+    for (let i = 0; i < lines.length; i++) {
+      if (i % 2 === 0) {
+        m3u_arr.push(
+          new M3uItem(
+            lines[i + 1],
+            lines[i].replace(/#EXTINF:-1,|#EXTINF:-1/g, ""),
+          ),
+        );
+      }
+    }
+    // console.log(m3u_arr);
+    return m3u_arr;
+  } else {
+    return [];
+  }
+}
+
+export class M3uItem {
+  url: string;
+  name: string;
+  constructor(url: string, name: string) {
+    this.url = url;
+    this.name = name;
+  }
+}
